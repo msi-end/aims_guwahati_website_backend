c9b5acd12bb6e2c3635cfd0b6ec1444f51cbac5b
Panchanan Deka <111901167+panchanan16@users.noreply.github.com>
2026-01-21 14:28:03 +0530

    Update CORS middleware configuration

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.set("layout extractStyles", true);
 // ===================
 // MIDDLEWARE
 // ===================
-// app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
